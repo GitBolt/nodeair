7a814cb51f91e4c4f2b0503dd1b1895e9aab73af
GitBolt <stingbolt1@gmail.com>
2021-10-21 23:09:53 +0530

    🔒 Basic address validation

--- maskpay/routers/auth.py
+++ maskpay/routers/auth.py
@@ -1,11 +1,18 @@
 from fastapi import APIRouter
 from core.models import User
 from core.db import USERS
+from solana.rpc.async_api import AsyncClient
 
 router = APIRouter()
 
 @router.post("/register")
 async def register(user: User):
     user = user.dict()
-    await USERS.insert_one(user)
+    async with AsyncClient("https://api.devnet.solana.com") as client:
+        verify = await client.get_balance(user["address"])
+        if "error" in verify.keys():
+            return {"error": "Invalid address"} # Big brain right here
+        else:
+            await USERS.insert_one(user)
+            return {"message": f"Successfully registered: {user['address']}"}
 
