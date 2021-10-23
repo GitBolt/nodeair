ad77a92bfc00f376551284d6ad53b45fcc55231c
GitBolt <stingbolt1@gmail.com>
2021-10-24 01:00:03 +0530

    ✨ Beautify code

--- pay/main.py
+++ pay/main.py
@@ -1,3 +1,4 @@
+from typing import Union
 from fastapi import FastAPI, HTTPException
 from fastapi.param_functions import Depends
 from sqlalchemy.orm import Session
@@ -10,26 +11,32 @@ from db import engine, SessionLocal, Base
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-def get_db():
+def get_db() -> None:
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
 
-
 @app.get("/")
-async def root():
+async def root() -> dict:
     return {"message": "Welcome"}
 
 @app.post("/register")
-async def register(user: RegisterUser, 
-                    db: Session = Depends(get_db)):
-    get_user = db.query(User).filter(User.public_key == user.public_key).first()
+async def register(
+                user: RegisterUser, db: Session=Depends(get_db)
+                ) -> Union[HTTPException, dict]:
+
+    get_user = db.query(User).filter(
+                User.public_key == user.public_key).first()
+
     if get_user:
-        raise HTTPException(status_code=400, detail="Public key already registered")
+        raise HTTPException(
+                        status_code=400, 
+                        detail="Public key already registered."
+                        )
     else:
-        db_user = User(username=user.username, public_key = user.public_key)
+        db_user = User(username=user.username, public_key=user.public_key)
         db.add(db_user)
         db.commit()
         db.refresh(db_user)
--- pay/schemas.py
+++ pay/schemas.py
@@ -1,7 +1,11 @@
 from datetime import datetime
-from sqlalchemy import (Column, String, 
-                        Integer, DateTime, 
-                        ForeignKey)
+from sqlalchemy import (
+                        Column, 
+                        String, 
+                        Integer, 
+                        DateTime, 
+                        ForeignKey
+                        )
 from sqlalchemy.orm import relationship
 from db import Base
 
@@ -9,14 +13,14 @@ from db import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     username = Column(String(length=15))
     public_key = Column(String(length=44))
     joined_at = Column(DateTime, default=datetime.utcnow)
-    avatar = Column(String, 
-                    default=("https://cdn.discordapp.com/attachments/"
-                            "807140294764003350/901514551890178058/avatar.png")
-                    )
+    avatar = Column(String(100), default=(
+                    "https://res.cloudinary.com/f22/image"
+                    "/upload/v1635015065/Test/avatar.png"
+                    ))
 
     links = relationship("Link", back_populates="owner")
 
@@ -25,8 +29,8 @@ class Link(Base):
     __tablename__ = "links"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    link = Column(String)
-    expiration = Column(DateTime)
+    link = Column(String(30))
+    expiration = Column(DateTime, nullable=True)
     uses = Column(Integer, default=0)
     owner_id = Column(Integer, ForeignKey("users.id"))
 
