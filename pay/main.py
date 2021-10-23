f6a63e1b15e665905733f9d506532aa8deb47556
GitBolt <stingbolt1@gmail.com>
2021-10-24 01:53:49 +0530

    🚀 Added rate limiting

--- pay/main.py
+++ pay/main.py
@@ -1,14 +1,15 @@
 from typing import Union
+from aioredis import from_url
 from fastapi import FastAPI, HTTPException
 from fastapi.param_functions import Depends
 from sqlalchemy.orm import Session
 
 from schemas import User
+from ratelimit import RateLimit, RateLimiter
 from models import RegisterUser
 from db import engine, SessionLocal, Base
 
 
-Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 def get_db() -> None:
@@ -18,11 +19,24 @@ def get_db() -> None:
     finally:
         db.close()
 
-@app.get("/")
+Base.metadata.create_all(bind=engine)
+
+@app.on_event("startup")
+async def startup() -> None:
+    redis = await from_url("redis://localhost", encoding="utf8")
+    await RateLimiter.init(redis)
+
+@app.on_event("shutdown")
+async def shutdown() -> None:
+    await RateLimiter.close()
+
+
+
+@app.get("/", dependencies=[Depends(RateLimit(times=100, seconds=5))])
 async def root() -> dict:
     return {"message": "Welcome"}
 
-@app.post("/register")
+@app.post("/register", dependencies=[Depends(RateLimit(times=100, seconds=5))])
 async def register(
                 user: RegisterUser, db: Session=Depends(get_db)
                 ) -> Union[HTTPException, dict]:
