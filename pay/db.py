621f54b588fe2100a1743eca9c31c0abf92e159c
GitBolt <stingbolt1@gmail.com>
2021-10-24 00:42:52 +0530

    🔧 Handle DB_URL env variable exception

--- pay/db.py
+++ pay/db.py
@@ -1,11 +1,19 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DB_URL = "sqlite:///../db.sql"
-# DB_URL = mysql://username:password@host/db"
+def get_db_url() -> str:
+    try:
+        return os.environ["DB_URL"]
+    except KeyError:
+        print(
+            "'DB_URL' environment variable not found,",
+            "creating 'db.sql locally...'"
+            )
+        return "sqlite:///../db.sql"
 
-engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
+engine = create_engine(get_db_url())
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
