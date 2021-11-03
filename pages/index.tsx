02b667123cdc9067be6f5b291b4411e8cbb32c0c
GitBolt <stingbolt1@gmail.com>
2021-11-03 14:26:44 +0530

    ⏹️  Better buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Index() {
         <main className={styles.index_body}>
           <h1 className={styles.heading}>Customize your wallet, look how you want to be.</h1>
           <p className={styles.description}>Get more than a simple wallet address, customize and share your Solana identity with anyone in minutes.</p>
-          <button className={styles.create_profile} onClick={registerWallet}>Create your profile</button>
+          <button className={styles.button} onClick={registerWallet}>Create your profile</button>
           <div className={styles.profile_image} ><Image src={ProfileImage} height="600"/></div>
         </main>
     </>
