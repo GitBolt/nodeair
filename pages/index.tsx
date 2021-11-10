a1fc3e5da6f7e3398b976c4df3c5024ea4184d05
GitBolt <saabisakhtar@gmail.com>
2021-11-10 05:24:03 -0800

    ✨  Initialize new design

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,7 @@ import { DefaultHead } from '@/components/Head'
 import { Navbar } from '@/components/Navbar'
 import { registerWallet } from '@/components/Wallet'
 import styles from '@/styles/modules/Index.module.scss'
-import ProfileImage from '@/images/profileimage.svg';
-import GraphImagefrom from '@/images/graphimage.svg';
+import InsightsImage from '@/images/InsightsImage.png';
 
 export default function Index() {  
   return (
@@ -12,11 +11,14 @@ export default function Index() {
       <DefaultHead title={'NodeAir - Have more than just wallet address.'} />
         <Navbar/>
         <main className={styles.index_body}>
-          <h1 className={styles.heading}>Customize your wallet, get detailed insights about your Solana identity.</h1>
+          <h1 className={styles.heading}>Get more out of your Solana wallet.</h1>
           <p className={styles.description}>NodeAir let's you create your public wallet profile to find each other easily and get statistics with graphs about your transactions and activity in seconds. </p>
           <button className={styles.button} onClick={registerWallet}>Create your profile</button>
-          <div className={styles.profile_image} ><Image src={ProfileImage}/></div>
-          <div className={styles.graph_image} ><Image src={GraphImagefrom} height="400" width="850"/></div>
+          <div className={styles.profile_image} ><Image src={InsightsImage}/></div>
+          <div className={styles.features}>
+            <h2 className={styles.content_heading}>Features you want</h2>
+            <p className={styles.content_desc}>Easy to use, quick and useful features.</p>
+          </div>
         </main>
     </>
   )
