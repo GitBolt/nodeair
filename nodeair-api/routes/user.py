c28e27f4ecc05083dbe7fd033e27d9737bff4ec3
GitBolt <saabisakhtar@gmail.com>
2021-11-16 16:39:31 -0800

    🚀 Add validation check API

--- nodeair-api/routes/user.py
+++ nodeair-api/routes/user.py
@@ -72,3 +72,30 @@ async def profile(
         status_code=404,
         content="User not found"
     )
+
+@router.post("/check", 
+            dependencies=[Depends(RateLimit(times=20, seconds=5))],
+            status_code=200
+            )
+async def check(
+                user: RegisterUser, db: Session=Depends(get_db)
+                ) -> JSONResponse:
+
+    get_user = db.query(User).filter(
+                                    (User.username == user.username) |
+                                    (User.public_key == user.public_key)
+                                    ).first()
+    if get_user:
+        if get_user.public_key == user.public_key:
+            field = "Public key"
+        else:
+            field = "Username"             
+        return JSONResponse(
+                status_code=400, 
+                content={"error": f"{field} already registered"}
+                )    
+ 
+    else:
+        return JSONResponse(
+            status_code=200
+        )
