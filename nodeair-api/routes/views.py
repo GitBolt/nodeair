845d92764f127038a3313a470ba680549d6bff82
GitBolt <saabisakhtar@gmail.com>
2021-11-18 00:41:47 -0800

    📝 Return empty dates

--- nodeair-api/routes/views.py
+++ nodeair-api/routes/views.py
@@ -1,10 +1,10 @@
 from datetime import datetime
+from calendar import monthrange
 from core.db import get_db
 from fastapi import APIRouter
 from sqlalchemy.orm import Session
 from core.ratelimit import RateLimit
 from fastapi import APIRouter, Depends
-from fastapi.responses import JSONResponse
 from core.schemas import View
 
 
@@ -16,13 +16,18 @@ router = APIRouter(prefix="/fetch")
 async def views(public_key: str, db: Session=Depends(get_db)) -> dict:
 
     views = db.query(View).filter_by(public_key=public_key)
-    current_month = [x.viewed_on for x in views if x.viewed_on.month == datetime.utcnow().month]
-
+    current_month = datetime.utcnow().month
+    month_data = [x.viewed_on for x in views if x.viewed_on.month == current_month]
+    amount_of_days = monthrange(2021, current_month)[1]
     data = {}
-    for i in current_month:
-        if i.day in data.keys():
-            data[i.day] += 1
+    for i in range(1, amount_of_days + 1):
+        if i not in [x.day for x in month_data]:
+            data.update({i: 0})
         else:
-            data.update({i.day: 1})
+            for d in month_data:
+                if d.day in data.keys():
+                    data[d.day] += 1
+                else:
+                    data.update({d.day: 1})
 
     return data
