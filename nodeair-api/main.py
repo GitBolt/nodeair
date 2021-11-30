5e26deacc5939699cfd82085ccce87f2e43d2552
GitBolt <saabisakhtar@gmail.com>
2021-12-01 01:52:06 +0530

    📶Guess what? CORS!

--- nodeair-api/main.py
+++ nodeair-api/main.py
@@ -21,7 +21,7 @@ load_dotenv()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://nodeair.io"],
+    allow_origins=["http://localhost:3000", "https://nodeair.io", "https://www.nodeair.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
