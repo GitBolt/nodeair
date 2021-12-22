ab7eb4a10308e342a12d9ca3f0b971f90421f5c0
GitBolt <saabisakhtar@gmail.com>
2021-12-23 02:20:30 +0530

    🔧 Refactor code & improve return data format

--- api/src/routes/transactions.py
+++ api/src/routes/transactions.py
@@ -1,20 +1,19 @@
-from fastapi import Request
 from datetime import datetime
-from calendar import monthrange
-from utils import lamport_to_sol
-from fastapi import APIRouter
-from core.ratelimit import Limit
-from fastapi import APIRouter, Depends
-from fastapi.responses import JSONResponse
-from typing import Optional
 
-router = APIRouter(prefix="/fetch")
+
+from datetime import datetime
+from fastapi.responses import JSONResponse
+from fastapi import Request, APIRouter, Depends
+from core.ratelimit import Limit
+from utils import lamport_to_sol
+
+router = APIRouter(prefix="/transactions")
 
 @router.get("/activity/{public_key}",
             dependencies=[Depends(Limit(times=30, seconds=5))],
             status_code=200
             )
-async def activity(public_key: str,  request: Request, limit: int = 4, split_message: bool = False) -> JSONResponse:
+async def activity(public_key: str,  request: Request, limit: int = 4) -> JSONResponse:
 
     resp = await request.app.request_client.get(
                 ("https://api.solscan.io/account/"
@@ -24,20 +23,12 @@ async def activity(public_key: str,  request: Request, limit: int = 4, split_mes
     data = resp.json()["data"]["tx"]["transactions"]
     filtered_data = []
     for i in data:
-
-        sols = lamport_to_sol(i["lamport"])
+        sols = round(lamport_to_sol(i["lamport"]), 4)
         if i["src"] == public_key:
-            if split_message:
-                d = {"type": "sent", "amount": sols, "to": i['dst'], "tx": i["txHash"]}
-            else:
-                d = {"type": "sent", "message": f"Sent {sols} SOLs to {i['dst']}", "tx": i["txHash"]}
-                
+            d = {"type": "sent", "amount": sols, "to": i['dst'], "tx": i["txHash"], "time": i["blockTime"]}
             filtered_data.append(d)
         else:
-            if split_message:
-                d = {"type": "received", "amount": sols, "from": i['src'], "tx": i["txHash"]}
-            else:
-                d = {"type": "received", "message": f"Received {sols} SOLs from {i['src']}", "tx": i["txHash"]}
+            d = {"type": "received", "amount": sols, "from": i['src'], "tx": i["txHash"], "time": i["blockTime"]}
             filtered_data.append(d)
 
 
