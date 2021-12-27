4a85f32bcbe0c1620f33174bda11adcd2822dc08
GitBolt <saabisakhtar@gmail.com>
2021-12-28 02:01:05 +0530

    🔧 Fix avatar not rounded

--- components/DiscoverProfiles.tsx
+++ components/DiscoverProfiles.tsx
@@ -51,7 +51,7 @@ export const DiscoverProfiles = () => {
       {data ? (data.map((a: any) => (
         <Link key={a['username']} href={a['username']} ><a className={styles.profileBox}>
           <div className={styles.upper}>
-            <Image src={a['avatar']} height="50" width="50" alt="avatar" />
+            <Image src={a['avatar']} className={styles.avatar} objectFit='cover' height="50px" width="50px"alt="avatar" />
             <div className={styles.nameGroup}>
               <span className={styles.name}>{a['name']}</span>
               <span className={styles.username}>@{a['username']} </span>
