c86dd7c304d15670c818b20153de8f0186eb671a
GitBolt <saabisakhtar@gmail.com>
2021-12-28 14:26:10 +0530

    🖍️ PEP8ified

--- api/src/routes/profile.py
+++ api/src/routes/profile.py
@@ -1,38 +1,41 @@
-import random
-import json
-from typing import Optional, Union
-
 import os
-import aiofiles
+import json
+import random
+from typing import Optional, Union
+from fastapi import (
+                    APIRouter, 
+                    Depends, 
+                    Request, 
+                    UploadFile, 
+                    File, 
+                    Form
+                    )
+from sqlalchemy.orm import Session
+from fastapi.responses import JSONResponse
 from core.db import get_db
 from core.ratelimit import Limit
-from sqlalchemy.orm import Session
-from core.schemas import User, View, Signature
 from core.models import ProfileFind
-from fastapi.responses import JSONResponse
-from fastapi import APIRouter, Depends, Request, UploadFile, File, Form
+from core.schemas import User, View, Signature
 from utils import lamport_to_sol, verify_signature
 
 router = APIRouter(prefix="/profile")
 
 
 @router.get("/{username}",
+            status_code=200,
             dependencies=[Depends(Limit(times=20, seconds=5))],
-            status_code=200
             )
-async def profile(
-        username: str,
-        request: Request,
-        db: Session = Depends(get_db),
-) -> Union[JSONResponse, User]:
+async def profile(username: str, request: Request, 
+                db: Session = Depends(get_db),
+                ) -> Union[JSONResponse, User]:
 
     user = db.query(User).filter_by(username=username).first()
 
     if user:
         resp = await request.app.request_client.get(
-            ("https://api.solscan.io/account/"
-             f"soltransfer/txs?address={user.public_key}&offset=0&limit={4}")
-        )
+            ("https://api.solscan.io/account/soltransfer/txs?"
+             f"address={user.public_key}&offset=0&limit={4}")
+            )
 
         data = resp.json()["data"]["tx"]["transactions"]
         filtered_data = []
@@ -59,22 +62,20 @@ async def profile(
 
 
 
-
-@router.put("/update",
-            dependencies=[Depends(Limit(times=20, seconds=5))],
-            status_code=200
-            )
+@router.put("/update", 
+            status_code=200,
+            dependencies=[Depends(Limit(times=20, seconds=5))])
 async def profile_update(
-    request: Request,
-    avatar: Optional[UploadFile] = File(None),
-    banner: Optional[UploadFile] = File(None),
-    name: Optional[str] = Form(None),
-    bio: Optional[str] = Form(None),
-    social: Optional[str] = Form(None),
-    public_key: str = Form(...),
-    signature: str = Form(...),
-    db: Session = Depends(get_db),
-    ) -> Union[JSONResponse, User]:
+            request: Request,
+            avatar: Optional[UploadFile] = File(None),
+            banner: Optional[UploadFile] = File(None),
+            name: Optional[str] = Form(None),
+            bio: Optional[str] = Form(None),
+            social: Optional[str] = Form(None),
+            public_key: str = Form(...),
+            signature: str = Form(...),
+            db: Session = Depends(get_db),
+            ) -> Union[JSONResponse, User]:
 
     signature = json.loads(signature)
     signature = signature["data"]
@@ -87,16 +88,16 @@ async def profile_update(
     verify = verify_signature(message_hash, signature, public_key)
     if not verify:
         return JSONResponse(
-            status_code=400,
+            status_code=403,
             content={"error": "Error verifying signature"}
         )
     else:
         user = db.query(User).filter_by(public_key=public_key).first()
         if not user:
             return JSONResponse(
-                status_code=400,
+                status_code=401,
                 content={
-                    "error": "You need to register your wallet in order to update profile"}
+                    "error": "Wallet not registered"}
             )
         user.name = name
         user.bio = bio
@@ -125,22 +126,17 @@ async def profile_update(
     db.refresh(user)
     return user
 
-
 @router.get("/update_count/{username}",
             dependencies=[Depends(Limit(times=5, seconds=5))],
-            status_code=200
             )
-async def update_count(
-    username: str,
-    db: Session = Depends(get_db),
-) -> Union[JSONResponse, User]:
+async def update_count(username: str,db: Session = Depends(get_db),
+                        ) -> JSONResponse:
 
     user = db.query(User).filter_by(username=username).first()
     if user:
         view = View(public_key=user.public_key)
         db.add(view)
         db.commit()
-        return {"message": f"View updated for {username}"}
 
     return JSONResponse(
         status_code=404,
@@ -148,30 +144,35 @@ async def update_count(
     )
 
 
-@router.post("/ext/find",
-             dependencies=[Depends(Limit(times=20, seconds=5))],
-             status_code=200)
-async def find(profilefind: ProfileFind, db: Session = Depends(get_db)) -> dict:
-    public_key_search = db.query(User).filter_by(
-        public_key=profilefind.username_or_public_key).first()
-    if public_key_search:
-        return public_key_search
+@router.post("/ext/find", 
+            status_code=200,
+            dependencies=[Depends(Limit(times=20, seconds=5))])
+async def find(profilefind: ProfileFind, db: Session = Depends(get_db)
+                ) -> Union[JSONResponse, User]:
 
-    username_search = db.query(User).filter_by(
-        username=profilefind.username_or_public_key).first()
-    if username_search:
-        return username_search
+    pub_key_find = db.query(User).filter_by(public_key=
+                                            profilefind.username_or_public_key
+                                            ).first()
+    if pub_key_find:
+        return pub_key_find
+
+    username_find = db.query(User).filter_by(username=
+                                            profilefind.username_or_public_key
+                                            ).first()
+    if username_find:
+        return username_find
 
     return JSONResponse(
-        status_code=400,
+        status_code=404,
         content={"error": "User not found"}
     )
 
 
-@router.get("/ext/getrandom",
-            dependencies=[Depends(Limit(times=20, seconds=5))],
-            status_code=200)
-async def getrandom(limit: int = 5, db: Session = Depends(get_db)) -> dict:
+@router.get("/ext/getrandom", 
+            status_code=200,
+            dependencies=[Depends(Limit(times=20, seconds=5))])
+async def getrandom(limit: int = 5, db: Session = Depends(get_db)
+                    ) -> Union[JSONResponse, list]:
     if limit > 100:
         return JSONResponse(
             status_code=400,
