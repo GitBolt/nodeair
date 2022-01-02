d6e486d8255fd1fe1e198d740ada81ef58344f3c
GitBolt <saabisakhtar@gmail.com>
2022-01-02 23:42:17 +0530

    ✨ Added get NFT API

--- api/src/routes/fetch.py
+++ api/src/routes/fetch.py
@@ -10,7 +10,7 @@ from solana.rpc.types import TokenAccountOpts
 from core.db import get_db
 from core.ratelimit import Limit
 from core.schemas import View
-from utils import lamport_to_sol
+from utils import lamport_to_sol, get_nft_metadata
 
 router = APIRouter(prefix="/fetch")
 
@@ -45,7 +45,7 @@ async def views(public_key: str, db: Session = Depends(get_db)) -> dict:
 @router.get("/tokens/{public_key}",
             dependencies=[Depends(Limit(times=5, seconds=10))],
             status_code=200)
-async def views(request: Request, public_key: str) -> JSONResponse:
+async def tokens(request: Request, public_key: str) -> JSONResponse:
     SOL_ADDRESS = "So11111111111111111111111111111111111111112"
     tokens = await request.app.solana_client.get_token_accounts_by_owner(
             public_key,
@@ -119,66 +119,35 @@ async def views(request: Request, public_key: str) -> JSONResponse:
 @router.get("/nfts/{public_key}",
             dependencies=[Depends(Limit(times=5, seconds=10))],
             status_code=200)
-async def views(request: Request, public_key: str) -> JSONResponse:
-    SOL_ADDRESS = "So11111111111111111111111111111111111111112"
+async def nfts(request: Request, public_key: str) -> JSONResponse:
+
     tokens = await request.app.solana_client.get_token_accounts_by_owner(
             public_key,
             TokenAccountOpts(
                 program_id='TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA',
                 encoding="jsonParsed"),
             "max")
-    
-    public_keys = [i["account"]["data"]["parsed"]["info"]["mint"]
-                   for i in tokens["result"]["value"]]
-    public_keys.append(SOL_ADDRESS)
+    try:
+        public_keys = [i["account"]["data"]["parsed"]["info"]["mint"]
+                    for i in tokens["result"]["value"]]
+    except Exception:
+        print("Execption with tokens: ", tokens)
+        return JSONResponse(
+                status_code=500,
+                content={"error": "Error fetching tokens, try reloading."}
+                )
 
     possible_nfts = [i["account"]["data"]["parsed"]["info"]["mint"]
                    for i in tokens["result"]["value"] if i["account"]["data"]["parsed"]["info"]["tokenAmount"]["uiAmount"] == 1]
 
-    amounts = [i["account"]["data"]["parsed"]["info"]["tokenAmount"]
-               ["uiAmount"] for i in tokens["result"]["value"]]
-    sol_balance = await request.app.solana_client.get_balance(public_key)
-    amounts.append(lamport_to_sol(sol_balance["result"]["value"]))    
-    amounts_pair = {i:y for (i,y) in zip(public_keys, amounts)}
-
     file = open("assets/tokens.json", "r", encoding='utf-8')
     token_data = json.loads(file.read())
     file.close()
-    
+
     token_public_keys = [x for x in public_keys if x in [y["address"] for y in token_data]]
     nfts = [i for i in possible_nfts if i not in token_public_keys]
-    prices = await request.app.request_client.get("https://api.coingecko.com/api/v3/simple/token_price/solana"+
-                                                    f"?contract_addresses={','.join(token_public_keys)}&vs_currencies=usd")          
-    prices_data = prices.json()
-    unfetched_public_keys = [x for x in token_public_keys if x not in [y for y in prices_data]]
-    unfetched_coingecko_ids = {}
-    for i in token_data:
-        if i["address"] in unfetched_public_keys:
-            try:
-                unfetched_coingecko_ids.update({i["extensions"]["coingeckoId"]: i["address"] })
-            except:
-                continue
-
-    new_prices = await request.app.request_client.get("https://api.coingecko.com/api/v3/simple/price/"+
-                                                    f"?ids={','.join(unfetched_coingecko_ids.keys())}&vs_currencies=usd") 
-    new_data = new_prices.json()
-    for i in list(new_data):
-        new_data[unfetched_coingecko_ids[i]] = new_data.pop(i)
-
-    prices_data.update(new_data)
-    sol_price = prices_data[SOL_ADDRESS]["usd"]
-    for i in prices_data:
-        token = [x for x in token_data if x["address"] == i][0]
-        token_price = prices_data[i]
-        token_price.update({"symbol": token["symbol"], "logo": token["logoURI"], "address": token["address"]})
-        token_price["usd"] = round(token_price["usd"] * amounts_pair[token_price["address"]], 2)
-
-    token_values = OrderedDict(sorted(prices_data.items(), key = lambda x: getitem(x[1], 'usd'), reverse=True))
-    data = {"tokenValues": token_values,
-            "nftCount": len(nfts), 
-            "fungibleTokenCount": len(token_public_keys), 
-            "unavailableTokenCount": len(token_public_keys) - len(token_values), 
-            "solPrice": sol_price,
-            "walletValue": round(sum([value["usd"] for key,value in token_values.items()]), 2)}
-            
+    data = []
+    for i in nfts:
+        x = await get_nft_metadata(request.app.solana_client, i)
+        data.append(x)
     return data
--- api/src/utils.py
+++ api/src/utils.py
@@ -1,11 +1,17 @@
 from nacl.signing import VerifyKey
 from solana.publickey import PublicKey
 import random
+import base58
+import base64
+import struct
+from solana.rpc.api import Client
+from solana.publickey import PublicKey
+
 
 def lamport_to_sol(lamports: float) -> float:
     return float(lamports / 1000000000)
 
-def verify_signature(hash, signature, public_key) -> bool:
+def verify_signature(hash: str, signature: list, public_key: str) -> bool:
     try:
         vk = VerifyKey(bytes(PublicKey(public_key)))
         vk.verify(bytes(hash, encoding="utf8"), bytes(signature))
@@ -22,3 +28,71 @@ def get_random_banner():
     base_url = "https://res.cloudinary.com/f22/image/upload/v1638256420/NodeAir/"
     options = ("1b", "2b", "3b", "4b", "5b")
     return base_url + random.choice(options) + ".png"
+
+
+def unpack_metadata_account(data):
+    assert(data[0] == 4)
+    i = 1
+    source_account = base58.b58encode(bytes(struct.unpack('<' + "B"*32, data[i:i+32])))
+    i += 32
+    mint_account = base58.b58encode(bytes(struct.unpack('<' + "B"*32, data[i:i+32])))
+    i += 32
+    name_len = struct.unpack('<I', data[i:i+4])[0]
+    i += 4
+    name = struct.unpack('<' + "B"*name_len, data[i:i+name_len])
+    i += name_len
+    symbol_len = struct.unpack('<I', data[i:i+4])[0]
+    i += 4 
+    symbol = struct.unpack('<' + "B"*symbol_len, data[i:i+symbol_len])
+    i += symbol_len
+    uri_len = struct.unpack('<I', data[i:i+4])[0]
+    i += 4 
+    uri = struct.unpack('<' + "B"*uri_len, data[i:i+uri_len])
+    i += uri_len
+    fee = struct.unpack('<h', data[i:i+2])[0]
+    i += 2
+    has_creator = data[i] 
+    i += 1
+    creators = []
+    verified = []
+    share = []
+    if has_creator:
+        creator_len = struct.unpack('<I', data[i:i+4])[0]
+        i += 4
+        for _ in range(creator_len):
+            creator = base58.b58encode(bytes(struct.unpack('<' + "B"*32, data[i:i+32])))
+            creators.append(creator)
+            i += 32
+            verified.append(data[i])
+            i += 1
+            share.append(data[i])
+            i += 1
+    primary_sale_happened = bool(data[i])
+    i += 1
+    is_mutable = bool(data[i])
+    metadata = {
+        "update_authority": source_account,
+        "mint": mint_account,
+        "data": {
+            "name": bytes(name).decode("utf-8").strip("\x00"),
+            "symbol": bytes(symbol).decode("utf-8").strip("\x00"),
+            "uri": bytes(uri).decode("utf-8").strip("\x00"),
+            "seller_fee_basis_points": fee,
+            "creators": creators,
+            "verified": verified,
+            "share": share,
+        },
+        "primary_sale_happened": primary_sale_happened,
+        "is_mutable": is_mutable,
+    }
+    return metadata
+
+def get_metadata_account(mint_key):
+    METADATA_PROGRAM_ID = PublicKey('metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s')
+    return PublicKey.find_program_address([b'metadata', bytes(METADATA_PROGRAM_ID), bytes(PublicKey(mint_key))], METADATA_PROGRAM_ID)[0]
+
+async def get_nft_metadata(client, mint_address: str) -> dict:
+    metadata_account = get_metadata_account(mint_address)
+    nft_info = await client.get_account_info(metadata_account)
+    data = base64.b64decode(nft_info['result']['value']['data'][0])
+    return unpack_metadata_account(data)
