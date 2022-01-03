d549001acf247b3ccfea6c72188c7707f6352eac
GitBolt <saabisakhtar@gmail.com>
2022-01-03 11:39:59 +0530

    🌀 Finish NFT item

--- components/NFT.tsx
+++ components/NFT.tsx
@@ -1,8 +1,10 @@
+import Image from 'next/image'
+import NFTBottom from '@/images/NFTBottom.svg'
+import Attributes from '@/images/icons/Attributes.svg'
 import styles from '@/styles/pages/NFTGallery.module.scss'
 
 export const NFT = ({ data }: any) => {
     if (data != null) {
-        console.log(data.attributes)
         return (
             <a className={styles.nft} href={data.image}>
                 <div className={styles.image}>
@@ -11,14 +13,20 @@ export const NFT = ({ data }: any) => {
                 <p className={styles.name} >{data.name}</p>
                 <p className={styles.description} >{data.description}</p>
                 <div className={styles.attributes}>
-                    <p>Attributes</p>
+                    <div className={styles.attributeTitle}>
+                        <Image src={Attributes} height="20px" width="20px"/>
+                        <p>Attributes</p>
+                    </div>
                     {data.attributes.map((a: any) => (
                         <div className={styles.attribute}>
                             <span>{a.trait_type}</span>
                             <span>{a.value}</span>
                         </div>
-                        ))}
+                    ))}
 
+                </div>
+                <div className={styles.nftBottom}>
+                    <Image src={NFTBottom} />
                 </div>
             </a>
 
